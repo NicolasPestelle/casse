59e6dad3f234eba8752dd7b6eb380f81d3214907
NicolasPestelle <nicolas.pestelle@hotmail.fr>
2017-03-07 10:25:11 +0100

    changeCasse entree std

--- changeCasse.cpp
+++ changeCasse.cpp
@@ -2,18 +2,26 @@
 // g++ -std=c++11 -Wall -Wextra -o changeCasse.out changeCasse.cpp
 #include <cctype>
 #include <iostream>
-int main()
+int main(int argc, char ** argv)
 {
-    std::string texte = "Toto, Tata";
+  //std::string texte = "Toto, Tata";
     // change les minuscules par des majuscules et réciproquement
-    for (char & c : texte)
+  char *tmp; 
+  for(int j=1; j<argc; j++)
     {
-        if (std::islower(c))
-            std::cout << char(std::toupper(c));
-        else if (std::isupper(c))
-            std::cout << char(std::tolower(c));
-        else 
-            std::cout << c;
+      tmp = argv[j];
+      
+      for (int i =0; i<sizeof(tmp); i++)
+      {
+          if (std::islower(tmp[i]))
+              std::cout << char(std::toupper(tmp[i]));
+          else if (std::isupper(tmp[i]))
+              std::cout << char(std::tolower(tmp[i]));
+          else 
+              std::cout << tmp[i];
+      }
     }
+  
+
     return 0;
 }
